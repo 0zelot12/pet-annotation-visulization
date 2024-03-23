a79895cc01fa73c0c0874523bb3cc9927d0122b3
alauritz <lauritz.andreas@gmail.com>
2024-03-23 23:10:23 +0100

    Remove styles for now

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,11 @@ interface Entity {
 
 export function Annotation({ text, type }: { text: string, type: string }) {
   if (type === "ACTIVITY") {
-    return <Text as='span' backgroundColor='teal.100' paddingX={1} rounded={2}>{text}</Text>
+    return <Text as='span' backgroundColor='teal.100'>{text}</Text>
   } else if (type === "ACTIVITY_DATA") {
-    return <Text as='span' backgroundColor='orange.100' paddingX={1} rounded={2}>{text}</Text>
+    return <Text as='span' backgroundColor='orange.100'>{text}</Text>
   } else if (type === "ACTOR") {
-    return <Text as='span' backgroundColor='red.100' paddingX={1} rounded={2}>{text}</Text>
+    return <Text as='span' backgroundColor='red.100'>{text}</Text>
   } else {
     return <Text as='span'>{text}</Text>
   }
